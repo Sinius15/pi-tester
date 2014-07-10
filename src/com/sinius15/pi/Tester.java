e4b3d72563f0d5d967d7ef777564b362e8372b95
sinius15 <sinius15@gmail.com>
2014-07-10 20:06:00 +0200

    pudate

--- src/com/sinius15/pi/Tester.java
+++ src/com/sinius15/pi/Tester.java
@@ -1,52 +1,55 @@
 package com.sinius15.pi;
 
-import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
 import com.pi4j.io.gpio.PinPullResistance;
 import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.gpio.impl.GpioControllerImpl;
 
 public class Tester {
 	
 	public static void main(String[] args) {
 		
-		GpioController gpio = GpioFactory.getInstance();
+		GpioControllerImpl gpio = (GpioControllerImpl) GpioFactory.getInstance();
 		
 		GpioPinDigitalOutput[] outs = new GpioPinDigitalOutput[] {
-				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "gpio 0", PinState.LOW),
-				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "gpio 1", PinState.LOW),
-				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, "gpio 2", PinState.LOW),
-				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, "gpio 3", PinState.LOW),
-				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "gpio 4", PinState.LOW),
-				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05, "gpio 5", PinState.LOW),
-				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_06, "gpio 6", PinState.LOW),
-				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_07, "gpio 7", PinState.LOW),
-				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_08, "gpio 8", PinState.LOW),
-				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_09, "gpio 9", PinState.LOW),
-				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_10, "gpio 10", PinState.LOW),
-				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_11, "gpio 11", PinState.LOW),
-				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_12, "gpio 12", PinState.LOW),
-				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_13, "gpio 13", PinState.LOW),
-				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_14, "gpio 14", PinState.LOW),
-				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_15, "gpio 15", PinState.LOW),
-				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_16, "gpio 16", PinState.LOW),
-				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_17, "gpio 17", PinState.LOW),
-				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_18, "gpio 18", PinState.LOW),
-				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_19, "gpio 19", PinState.LOW),
-				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_20, "gpio 20", PinState.LOW),
+				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "gpio 0"),
+				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "gpio 1"),
+				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, "gpio 2"),
+				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, "gpio 3"),
+				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "gpio 4"),
+				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05, "gpio 5"),
+				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_06, "gpio 6"),
+				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_07, "gpio 7"),
+				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_08, "gpio 8"),
+				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_09, "gpio 9"),
+				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_10, "gpio 10"),
+				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_11, "gpio 11"),
+				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_12, "gpio 12"),
+				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_13, "gpio 13"),
+				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_14, "gpio 14"),
+				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_15, "gpio 15"),
+				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_16, "gpio 16"),
+				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_17, "gpio 17"),
+				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_18, "gpio 18"),
+				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_19, "gpio 19"),
+				gpio.provisionDigitalOutputPin(RaspiPin.GPIO_20, "gpio 20"),
 		
 		};
 		for (GpioPinDigitalOutput o : outs) {
 			o.setShutdownOptions(true, PinState.LOW, PinPullResistance.OFF);
+		}
+		
+		for (GpioPinDigitalOutput o : outs) {
 			System.out.println("turned on: " + o.getName());
-			o.setState(PinState.HIGH);
+			o.setState(true);
 			try {
 				Thread.sleep(2000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			o.setState(PinState.LOW);
+			o.setState(false);
 		}
 		
 		gpio.shutdown();
