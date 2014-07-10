39dac790a56217f8860d851e65f0b1c812997731
sinius15 <sinius15@gmail.com>
2014-07-10 22:17:48 +0200

    auto commit on 10.07.2014 - 22:17:47

--- pi.py
+++ pi.py
@@ -18,42 +18,16 @@ GPIO.setup(22, GPIO.OUT)
 print "done setting up!" 
 
 for i in pinList: 
-    print "1"
     GPIO.output(i, GPIO.HIGH)
-    print "2"
+    time.sleep(1);
+    print "set to high"
 
-# time to sleep between operations in the main loop
-print "3"
-SleepTime = 1
-count = 1
-print "4"
-# main loop
-print "5"
-try:
-  for i in pinList:
-      print "6"
-      GPIO.output(i, GPIO.LOW)
-      print "7"
-      time.sleep(SleepTime);
-      print "8"
+for i in pinList:
+    GPIO.output(i, GPIO.LOW)
+    time.sleep(1);
+    print "est to low"
 
 
-  while (count > 0):
-    print "9"
-    pinList.reverse()
-    print "1"
+print "Good bye!"
 
-    for i in pinList:
-        print "2"
-        GPIO.output(i, GPIO.HIGH)
-        time.sleep(SleepTime);
-        print "3"
-
-  print "Good bye!"
-
-# End program cleanly with keyboard
-except KeyboardInterrupt:
-  print "  Quit"
-
-  # Reset GPIO settings
-  GPIO.cleanup()
+GPIO.cleanup()
