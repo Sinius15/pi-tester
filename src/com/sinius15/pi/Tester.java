45ca05c0c982b31eb97e148cdfa3f448afc8c124
sinius15 <sinius15@gmail.com>
2014-07-11 09:39:51 +0200

    auto commit on 11.07.2014 - 09:39:51

--- src/com/sinius15/pi/Tester.java
+++ src/com/sinius15/pi/Tester.java
@@ -2,38 +2,49 @@ package com.sinius15.pi;
 
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
-import com.pi4j.io.gpio.GpioPinDigitalInput;
-import com.pi4j.io.gpio.PinPullResistance;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
-import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
-import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 
 public class Tester {
 	
-	public static void main(String args[]) throws InterruptedException {
-        System.out.println("<--Pi4J--> GPIO Listen Example ... started.");
+public static void main(String[] args) throws InterruptedException {
+        
+        System.out.println("<--Pi4J--> GPIO Control Example ... started.");
         
         // create gpio controller
         final GpioController gpio = GpioFactory.getInstance();
-
-        // provision gpio pin #02 as an input pin with its internal pull down resistor enabled
-        final GpioPinDigitalInput myButton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02, PinPullResistance.PULL_DOWN);
-
-        // create and register gpio pin listener
-        myButton.addListener(new GpioPinListenerDigital() {
-            @Override
-            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
-                // display pin state on console
-                System.out.println(" --> GPIO PIN STATE CHANGE: " + event.getPin() + " = " + event.getState());
-            }
-            
-        });
         
-        System.out.println(" ... complete the GPIO #02 circuit and see the listener feedback here in the console.");
+        // provision gpio pin #01 as an output pin and turn on
+        final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.HIGH);
+        System.out.println("--> GPIO state should be: ON");
         
-        // keep program running until user aborts (CTRL-C)
-        for (;;) {
-            Thread.sleep(500);
-        }
-	}
+        Thread.sleep(5000);
+        
+        // turn off gpio pin #01
+        pin.low();
+        System.out.println("--> GPIO state should be: OFF");
+
+        Thread.sleep(5000);
+
+        // toggle the current state of gpio pin #01 (should turn on)
+        pin.toggle();
+        System.out.println("--> GPIO state should be: ON");
+
+        Thread.sleep(5000);
+
+        // toggle the current state of gpio pin #01  (should turn off)
+        pin.toggle();
+        System.out.println("--> GPIO state should be: OFF");
+        
+        Thread.sleep(5000);
+
+        // turn on gpio pin #01 for 1 second and then off
+        System.out.println("--> GPIO state should be: ON for only 1 second");
+        pin.pulse(1000, true); // set second argument to 'true' use a blocking call
+        
+        // stop all GPIO activity/threads by shutting down the GPIO controller
+        // (this method will forcefully shutdown all GPIO monitoring threads and scheduled tasks)
+        gpio.shutdown();
+    }
 }
