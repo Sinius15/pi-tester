55024fa6f05a3728a804a868ef4573a1d051eb16
sinius15 <sinius15@gmail.com>
2014-07-12 23:09:29 +0200

    default commit message. I probably did some sutff

--- src/com/sinius15/pi/remote/Remote.java
+++ src/com/sinius15/pi/remote/Remote.java
@@ -2,6 +2,9 @@ package com.sinius15.pi.remote;
 
 import java.io.IOException;
 
+import com.sinius15.pi.PiServer;
+import com.sinius15.pi.server.WireManager;
+
 import jline.ConsoleReader;
 
 public class Remote {
@@ -13,7 +16,7 @@ public class Remote {
 			cr = new ConsoleReader();
 			int car;
 			while ((car = cr.readVirtualKey()) != 0x09){
-				System.out.println(car);  
+				PiServer.wireManager.toggle(3);
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
