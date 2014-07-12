29dcef3593e5d6c3511772776c15848ac28579f5
sinius15 <sinius15@gmail.com>
2014-07-13 00:28:41 +0200

    default commit message. I probably did some sutff

--- src/com/sinius15/pi/remote/Remote.java
+++ src/com/sinius15/pi/remote/Remote.java
@@ -1,6 +1,8 @@
 package com.sinius15.pi.remote;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import jline.ConsoleReader;
 
@@ -40,23 +42,19 @@ public class Remote {
 		
 	}
 	
-	public static char mode(char[][] arr) {
-	    List<Integer> list = new ArrayList<Integer>();
+	public static char[] mode(char[][] arr) {
+	    List<Character> list = new ArrayList<Character>();
 	    for (int i = 0; i < arr.length; i++) {
-	        // tiny change 1: proper dimensions
 	        for (int j = 0; j < arr[i].length; j++) { 
-	            // tiny change 2: actually store the values
 	            list.add(arr[i][j]); 
 	        }
 	    }
 
-	    // now you need to find a mode in the list.
-
-	    // tiny change 3, if you definitely need an array
-	    int[] vector = new int[list.size()];
+	    char[] vector = new char[list.size()];
 	    for (int i = 0; i < vector.length; i++) {
 	        vector[i] = list.get(i);
 	    }
+	    return vector;
 	}
 	
 }
