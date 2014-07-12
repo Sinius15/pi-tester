b73b42e7251810c2b68f97602a874ae1275bde13
sinius15 <sinius15@gmail.com>
2014-07-13 00:22:36 +0200

    default commit message. I probably did some sutff

--- src/com/sinius15/pi/remote/Remote.java
+++ src/com/sinius15/pi/remote/Remote.java
@@ -8,13 +8,24 @@ import com.sinius15.pi.PiServer;
 
 public class Remote {
 	
+	char[][] things = new char[][]{
+			{'1','.','?'},
+			{'2','a','b', 'c', 'A', 'B', 'C'},
+			{'3','d','e', 'f', 'D', 'E', 'F'},
+			{'4','g','h', 'i', 'G', 'H', 'I'},
+			{'5','j','k', 'l', 'J', 'K', 'L'},
+			{'6','m','n', 'o', 'M', 'N', 'O'},
+			{'7','p','q', 'r', 's', 'P', 'Q', 'R', 'S'},
+			{'8','t','u', 'v', 'T', 'U', 'V'},
+			{'9','w','x', 'y', 'z', 'X', 'Y', 'Z', 'W'},
+	};
+	
 	public Remote(){
 		System.out.println("starting remote");
 		ConsoleReader cr;
 		try {
 			cr = new ConsoleReader();
 			
-			int car;
 			while (true){
 				PiServer.wireManager.toggle(3);
 			}
