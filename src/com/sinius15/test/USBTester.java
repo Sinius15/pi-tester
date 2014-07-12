ef3f19f2c585b92d0796dbde59c03f74a0588f7d
sinius15 <sinius15@gmail.com>
2014-07-12 21:11:33 +0200

    default commit message. I probably did some sutff

--- src/com/sinius15/test/USBTester.java
+++ src/com/sinius15/test/USBTester.java
@@ -2,16 +2,11 @@ package com.sinius15.test;
 
 import java.util.List;
 
-import javax.usb.UsbConfiguration;
-import javax.usb.UsbConst;
-import javax.usb.UsbControlIrp;
 import javax.usb.UsbDevice;
 import javax.usb.UsbDeviceDescriptor;
 import javax.usb.UsbException;
 import javax.usb.UsbHostManager;
 import javax.usb.UsbHub;
-import javax.usb.UsbInterface;
-import javax.usb.UsbInterfacePolicy;
 import javax.usb.event.UsbDeviceDataEvent;
 import javax.usb.event.UsbDeviceErrorEvent;
 import javax.usb.event.UsbDeviceEvent;
@@ -19,8 +14,8 @@ import javax.usb.event.UsbDeviceListener;
 
 public class USBTester implements UsbDeviceListener {
 	
-	public USBTester() throws SecurityException, UsbException{
-		UsbDevice device = findMissileLauncher(UsbHostManager.getUsbServices().getRootUsbHub());
+	public USBTester() throws SecurityException, UsbException {
+		UsbDevice device = findDevice(UsbHostManager.getUsbServices().getRootUsbHub());
 		if (device == null) {
 			System.err.println("Not Found");
 			System.exit(1);
@@ -28,18 +23,18 @@ public class USBTester implements UsbDeviceListener {
 		}
 		
 		device.addUsbDeviceListener(this);
-
+		
 	}
 	
 	private static final short VENDOR_ID = 0x0424;
 	
 	@SuppressWarnings("unchecked")
-	public static UsbDevice findMissileLauncher(UsbHub hub) {
+	public UsbDevice findDevice(UsbHub hub) {
 		UsbDevice launcher = null;
 		
 		for (UsbDevice device : (List<UsbDevice>) hub.getAttachedUsbDevices()) {
 			if (device.isUsbHub()) {
-				launcher = findMissileLauncher((UsbHub) device);
+				launcher = findDevice((UsbHub) device);
 				if (launcher != null)
 					return launcher;
 			} else {
@@ -50,26 +45,17 @@ public class USBTester implements UsbDeviceListener {
 		}
 		return null;
 	}
-
-	public static void sendMessage(UsbDevice device, byte[] message) throws UsbException {
-		UsbControlIrp irp = device.createUsbControlIrp(
-						(byte) (UsbConst.REQUESTTYPE_TYPE_CLASS | UsbConst.REQUESTTYPE_RECIPIENT_INTERFACE),
-						(byte) 0x09, (short) 2, (short) 1);
-		irp.setData(message);
-		device.syncSubmit(irp);
-	}
 	
-
+	@Override
+	public void dataEventOccurred(UsbDeviceDataEvent arg0) {}
+	
+	@Override
+	public void errorEventOccurred(UsbDeviceErrorEvent arg0) {}
+	
+	@Override
+	public void usbDeviceDetached(UsbDeviceEvent arg0) {}
+	
 	public static void main(String[] args) throws UsbException {
 		new USBTester();
 	}
-
-	@Override
-	public void dataEventOccurred(UsbDeviceDataEvent arg0) {}
-
-	@Override
-	public void errorEventOccurred(UsbDeviceErrorEvent arg0) {}
-
-	@Override
-	public void usbDeviceDetached(UsbDeviceEvent arg0) {}
 }
