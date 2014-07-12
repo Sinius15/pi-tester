fdbe567b80855ab64cdd74fe9cdb2d9afee9b36c
sinius15 <sinius15@gmail.com>
2014-07-13 00:27:29 +0200

    default commit message. I probably did some sutff

--- src/com/sinius15/pi/remote/Remote.java
+++ src/com/sinius15/pi/remote/Remote.java
@@ -8,7 +8,7 @@ import com.sinius15.pi.PiServer;
 
 public class Remote {
 	
-	char[][] things = new char[][]{
+	char[][] things2d = new char[][]{
 			{'1','.','?'},
 			{'2','a','b', 'c', 'A', 'B', 'C'},
 			{'3','d','e', 'f', 'D', 'E', 'F'},
@@ -19,20 +19,44 @@ public class Remote {
 			{'8','t','u', 'v', 'T', 'U', 'V'},
 			{'9','w','x', 'y', 'z', 'X', 'Y', 'Z', 'W'},
 	};
+	char[] things1d;
 	
 	public Remote(){
+		
+		things1d =  
+		
 		System.out.println("starting remote");
 		ConsoleReader cr;
 		try {
 			cr = new ConsoleReader();
 			
 			while (true){
+				char car = cr.readCharacter(things);
 				PiServer.wireManager.toggle(3);
 			}
 		} catch (IOException e) {
 			e.printStackTrace(); 
 		}
 		
+	}
+	
+	public static char mode(char[][] arr) {
+	    List<Integer> list = new ArrayList<Integer>();
+	    for (int i = 0; i < arr.length; i++) {
+	        // tiny change 1: proper dimensions
+	        for (int j = 0; j < arr[i].length; j++) { 
+	            // tiny change 2: actually store the values
+	            list.add(arr[i][j]); 
+	        }
+	    }
+
+	    // now you need to find a mode in the list.
+
+	    // tiny change 3, if you definitely need an array
+	    int[] vector = new int[list.size()];
+	    for (int i = 0; i < vector.length; i++) {
+	        vector[i] = list.get(i);
+	    }
 	}
 	
 }
