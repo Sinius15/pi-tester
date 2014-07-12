b6ccda6f5fffd6714d47228cc36504f59df0e576
sinius15 <sinius15@gmail.com>
2014-07-12 21:50:13 +0200

    default commit message. I probably did some sutff

--- src/com/sinius15/pi/remote/Remote.java
+++ src/com/sinius15/pi/remote/Remote.java
@@ -1,18 +1,27 @@
 package com.sinius15.pi.remote;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 public class Remote {
 	
 	public Remote(){
 		System.out.println("starting remote");
 		int in;
+		Reader input = new InputStreamReader(System.in);
 		try {
-			while((in = System.in.read()) != -1){
+			while((in = input.read()) != -1){
 				System.out.println(in);
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
+		}finally{
+			try {
+				input.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	
