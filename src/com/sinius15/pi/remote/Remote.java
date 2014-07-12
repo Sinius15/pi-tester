4748721f2ddceb9a00d1209c696b0cc9ea251b04
sinius15 <sinius15@gmail.com>
2014-07-13 00:52:03 +0200

    default commit message. I probably did some sutff

--- src/com/sinius15/pi/remote/Remote.java
+++ src/com/sinius15/pi/remote/Remote.java
@@ -10,58 +10,50 @@ import com.sinius15.pi.PiServer;
 
 public class Remote {
 	
-	static char[][] things2d = new char[][]{
-			{'1','.', ',', '?', '\t'},
-			{'2','a','b', 'c', 'A', 'B', 'C'},
-			{'3','d','e', 'f', 'D', 'E', 'F'},
-			{'4','g','h', 'i', 'G', 'H', 'I'},
-			{'5','j','k', 'l', 'J', 'K', 'L'},
-			{'6','m','n', 'o', 'M', 'N', 'O'},
-			{'7','p','q', 'r', 's', 'P', 'Q', 'R', 'S'},
-			{'8','t','u', 'v', 'T', 'U', 'V'},
-			{'9','w','x', 'y', 'z', 'X', 'Y', 'Z', 'W'},
-			{'0', ' ', '@', '*', '#', '_'} 
-	};
+	static char[][] things2d = new char[][] { { '1', '.', ',', '?', '\t' },
+			{ '2', 'a', 'b', 'c', 'A', 'B', 'C' }, { '3', 'd', 'e', 'f', 'D', 'E', 'F' },
+			{ '4', 'g', 'h', 'i', 'G', 'H', 'I' }, { '5', 'j', 'k', 'l', 'J', 'K', 'L' },
+			{ '6', 'm', 'n', 'o', 'M', 'N', 'O' }, { '7', 'p', 'q', 'r', 's', 'P', 'Q', 'R', 'S' },
+			{ '8', 't', 'u', 'v', 'T', 'U', 'V' }, { '9', 'w', 'x', 'y', 'z', 'X', 'Y', 'Z', 'W' },
+			{ '0', ' ', '@', '*', '#', '_' } };
 	static char[] things1d;
 	
 	static {
-		things1d =  mode(things2d);
+		things1d = mode(things2d);
 	}
 	
-	public Remote(){
-		
-		
+	public Remote() {
 		
 		System.out.println("starting remote");
 		ConsoleReader cr;
 		try {
 			cr = new ConsoleReader();
 			
-			while (true){
+			while (true) {
 				char in = (char) cr.readCharacter(things1d);
 				cr.printNewline();
-				cr.printString(Character.toString(in));  
-				PiServer.wireManager.toggle(3); 
+				cr.printString(Character.toString(in));
+				PiServer.wireManager.toggle(3);
 			}
 		} catch (IOException e) {
-			e.printStackTrace(); 
+			e.printStackTrace();
 		}
 		
 	}
 	
 	public static char[] mode(char[][] arr) {
-	    List<Character> list = new ArrayList<Character>();
-	    for (int i = 0; i < arr.length; i++) {
-	        for (int j = 0; j < arr[i].length; j++) { 
-	            list.add(arr[i][j]); 
-	        }
-	    }
-
-	    char[] vector = new char[list.size()];
-	    for (int i = 0; i < vector.length; i++) {
-	        vector[i] = list.get(i);
-	    }
-	    return vector;
+		List<Character> list = new ArrayList<Character>();
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				list.add(arr[i][j]);
+			}
+		}
+		
+		char[] vector = new char[list.size()];
+		for (int i = 0; i < vector.length; i++) {
+			vector[i] = list.get(i);
+		}
+		return vector;
 	}
 	
 }
