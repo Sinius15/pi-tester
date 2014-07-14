236a2d6cdca72b1037824ad6bce821ccca9d659a
sinius15 <sinius15@gmail.com>
2014-07-14 16:36:17 +0200

    default commit message. I probably did some sutff

--- src/com/sinius15/pi/controllers/Launcher.java
+++ src/com/sinius15/pi/controllers/Launcher.java
@@ -19,7 +19,7 @@ public class Launcher implements LaunchListener {
 		} 
 		try {
 			pad = new Launchpad("S [hw:1,0,0]");
-			pad.open();
+			pad.open(); 
 			pad.addListener(this);
 			pad.LEDTest();
 			PiServer.wireManager.onChangeListeners.add(onChange);
