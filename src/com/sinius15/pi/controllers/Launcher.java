5018dfba5c32503b91a8f6bd184b308d252ec121
sinius15 <sinius15@gmail.com>
2014-07-14 16:03:39 +0200

    default commit message. I probably did some sutff

--- src/com/sinius15/pi/controllers/Launcher.java
+++ src/com/sinius15/pi/controllers/Launcher.java
@@ -1,5 +1,26 @@
 package com.sinius15.pi.controllers;
 
-public class Launcher {
+import com.sinius15.launchpad.LaunchListener;
+import com.sinius15.launchpad.Launchpad;
+import com.sinius15.launchpad.LaunchpadException;
+
+public class Launcher implements LaunchListener{
+	
+	public Launcher(){
+		try {
+			Launchpad pad = new Launchpad("Lauchpad S");
+			pad.addListener(this);
+			pad.
+		} catch (LaunchpadException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public void onButtonDown(int row, int colomn) {}
+
+	@Override
+	public void onButtonUp(int row, int colomn) {}
 	
 }
