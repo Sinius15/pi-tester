7b2013f5b919a622a49fbd6a98592baddec95618
sinius15 <sinius15@gmail.com>
2014-07-14 16:19:05 +0200

    default commit message. I probably did some sutff

--- src/com/sinius15/pi/controllers/Launcher.java
+++ src/com/sinius15/pi/controllers/Launcher.java
@@ -5,28 +5,39 @@ import com.sinius15.launchpad.Launchpad;
 import com.sinius15.launchpad.LaunchpadException;
 import com.sinius15.pi.PiServer;
 
-public class Launcher implements LaunchListener{
+public class Launcher implements LaunchListener {
 	
 	public Launchpad pad;
 	
-	public Launcher(){
+	public Launcher() {
 		try {
 			pad = new Launchpad("Lauchpad S");
 			pad.addListener(this);
 		} catch (LaunchpadException e) {
 			e.printStackTrace();
 		}
-		
+		PiServer.wireManager.onChangeListeners.add(onChange);
 	}
-
+	
+	private Runnable onChange = new Runnable() {
+		@Override
+		public void run() {
+			pad.reset();
+			for(int i = 1; i <= 8; i++){
+				if(PiServer.wireManager.getState(i))
+					pad.setLedOn(i-1, 1, Launchpad.COLOUR_RED_FULL);
+			}
+		}
+	};
+	
 	@Override
 	public void onButtonDown(int row, int colomn) {
-		if(row == 1){
+		if (row == 1) {
 			colomn += 1;
 			PiServer.wireManager.toggle(colomn);
 		}
 	}
-
+	
 	@Override
 	public void onButtonUp(int row, int colomn) {}
 	
