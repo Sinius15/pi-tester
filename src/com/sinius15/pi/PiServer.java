931c6548229c8be3930077ad197be3297e12176c
sinius15 <sinius15@gmail.com>
2014-07-24 21:21:58 +0200

    default commit message. I probably did some sutff

--- src/com/sinius15/pi/PiServer.java
+++ src/com/sinius15/pi/PiServer.java
@@ -13,7 +13,7 @@ import com.sinius15.pi.services.WebsiteService;
 
 public class PiServer {
 	
-	public static final String VERSION = "2.2";
+	public static final String VERSION = "2.3";
 	public static final String LAUNCHPAD_NAME = "S [hw:1,0,0]";
 	public static final int WEB_SERVER_PORT = 80;
 	public static final int SOCKET_SERVER_PORT = 3443;
@@ -60,8 +60,7 @@ public class PiServer {
 	public static void startUpdating() {
 		String[] commands = new String[]{
 				"sudo java -jar updater.jar",
-				"sudo wget https://github.com/Sinius15/pi-tester/raw/master/pi.jar",
-				"sudo wget https://github.com/Sinius15/pi-tester/raw/master/latestVersion.txt",
+				"git pull",
 				"sudo reboot"
 		};
 		
