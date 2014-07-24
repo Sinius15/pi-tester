7ed03448c299bc6c11c745386108e7b757499454
sinius15 <sinius15@gmail.com>
2014-07-24 22:45:31 +0200

    default commit message. I probably did some sutff

--- src/com/sinius15/pi/WebUtil.java
+++ src/com/sinius15/pi/WebUtil.java
@@ -1,22 +1,28 @@
 package com.sinius15.pi;
 
-import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 
 public class WebUtil {
 	
 	public static String getWebContent(URL url) throws IOException {
-		URLConnection urlConn = url.openConnection();
-		BufferedInputStream inStream = new BufferedInputStream(urlConn.getInputStream());
+		URLConnection urlConnection = url.openConnection();
+		HttpURLConnection connection = null;
+		if (urlConnection instanceof HttpURLConnection)
+			connection = (HttpURLConnection) urlConnection;
+		else
+			throw new IOException("Connection was not an HTTP-URL-Connection!");
 		
-		StringBuilder out = new StringBuilder();
-		byte[] b = new byte[1024];
-		while(inStream.read(b) != -1)
-			out.append(new String(b));
-		inStream.close();
-		
-		return out.toString();
+		BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		String urlString = "";
+		String current;
+		while ((current = in.readLine()) != null) {
+			urlString += current;
+		}
+		return urlString;
 	}
 }
